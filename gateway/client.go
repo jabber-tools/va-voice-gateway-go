608d8da93d0e4436fda496d2756b321386217382
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2021-01-27 21:19:17 +0100

    gateway & client structs added

--- gateway/client.go
+++ gateway/client.go
@@ -1,1 +1,25 @@
 package gateway
+
+// TBD: rust version contains other attributes
+// we probably do not need them in Go solution
+type Client struct {
+	ClientId string
+	PlaybackId string
+	DoSTT bool
+	Dtmf []string
+	BotId string
+	Lang string
+	InviteParams map[string]string
+}
+
+func NewClient(clientId string, botId string, lang string, inviteParams map[string]string) Client {
+	return Client {
+		ClientId: clientId,
+		PlaybackId: nil,
+		DoSTT: false, // TBD: conditionally true for audio buffering feature
+		Dtmf: make([]string, 0),
+		BotId: botId,
+		Lang: lang,
+		InviteParams: inviteParams,
+	}
+}
--- gateway/gateway.go
+++ gateway/gateway.go
@@ -1,1 +1,25 @@
 package gateway
+
+type BotConfig struct {
+
+}
+
+type Gateway struct {
+	Clients map[string]Client
+	BotConfigs []BotConfig
+}
+
+func NewGateway(botConfigs []BotConfig) Gateway {
+	return Gateway{
+		Clients: make(map[string]Client),
+		BotConfigs: botConfigs,
+	}
+}
+
+func (g *Gateway) AddClient(client Client) {
+	g.Clients[client.ClientId] = client
+}
+
+func (g *Gateway) RemoveClient(client Client) {
+	delete(g.Clients, client.ClientId)
+}
