eed44208d62572e860a657b57289165fbf193233
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2021-02-10 22:00:24 +0100

    pm2 file fix

--- app.pm2.config.js
+++ app.pm2.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps: [
     {
       name: 'va-voice-gateway-go',
-      script: '/appl/va-voice-gateway-go/va-voice-gateway -f /appl/va-voice-gateway-go-conf/appconf.toml',
+      script: '/appl/va-voice-gateway-go/va-voice-gateway /appl/va-voice-gateway-go-conf/appconf.toml',
       exec_interpreter: "none",
       exec_mode  : "fork_mode",
       kill_timeout : 40000
