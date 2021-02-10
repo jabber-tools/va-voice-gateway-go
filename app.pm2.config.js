6e8a86cc6ce81482d1c28891c63bc9ca702cb306
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2021-02-10 21:49:13 +0100

    pm2 script added

---
+++ app.pm2.config.js
@@ -0,0 +1,12 @@
+// https://stackoverflow.com/questions/36290358/can-pm2-be-used-with-compiled-c-programs
+module.exports = {
+  apps: [
+    {
+      name: 'va-voice-gateway-go',
+      script: '/appl/va-voice-gateway-go/va-voice-gateway -f /appl/va-voice-gateway-go-conf/appconf.toml',
+      exec_interpreter: "none",
+      exec_mode  : "fork_mode",
+      kill_timeout : 40000
+    }
+  ]
+}
